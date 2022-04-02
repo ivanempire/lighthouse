7cb7bd04265c127012b4f3b31262ac9c3b7a8be7
Ivan Melnikov <ivanempire@gmail.com>
2022-04-02 01:53:09 -0700

    BYEBYE parsing implemented

--- lighthouse/src/main/java/com/ivanempire/lighthouse/models/LighthouseState.kt
+++ lighthouse/src/main/java/com/ivanempire/lighthouse/models/LighthouseState.kt
@@ -6,12 +6,18 @@ import com.ivanempire.lighthouse.models.packets.ByeByeMediaPacket
 import com.ivanempire.lighthouse.models.packets.MediaPacket
 import java.lang.IllegalStateException
 
-// BOOTID.UPNP.ORG     ==> changes, means device will reboot
+// BOOTID.UPNP.ORG     ==> changes, means device will reboot; see how to handle this
 // CONFIGID.UPNP.ORG   ==> changes, pull new XML description
 // NEXTBOOTID.UPNP.ORG ==> next bootId to use
 object LighthouseState {
 
     private val deviceList = mutableListOf<AbridgedMediaDevice>()
+
+    fun setDeviceList(updatedList: List<AbridgedMediaDevice>): List<MediaDevice> {
+        deviceList.clear()
+        deviceList.addAll(updatedList)
+        return deviceList
+    }
 
     fun parseMediaPacket(latestPacket: MediaPacket): List<MediaDevice> {
         return when (latestPacket) {
@@ -24,7 +30,7 @@ object LighthouseState {
 
     private fun parseByeByeMediaPacket(latestPacket: ByeByeMediaPacket): List<MediaDevice> {
         val targetDevice = deviceList.firstOrNull { it.uuid == latestPacket.uuid } ?: return deviceList
-
-        return emptyList()
+        deviceList.remove(targetDevice)
+        return deviceList
     }
 }
---
+++ lighthouse/src/test/java/com/ivanempire/lighthouse/models/LighthouseStateTest.kt
@@ -0,0 +1,32 @@
+package com.ivanempire.lighthouse.models
+
+import com.ivanempire.lighthouse.models.LighthouseStateTest.Fixtures.SHELL_BYEBYE_PACKET
+import com.ivanempire.lighthouse.models.packets.AliveMediaPacket
+import com.ivanempire.lighthouse.models.packets.ByeByeMediaPacket
+import com.ivanempire.lighthouse.models.packets.UpdateMediaPacket
+import java.util.UUID
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+class LighthouseStateTest {
+
+    private lateinit var sut: LighthouseState
+
+    @Test
+    fun `handles bye-bye packets correctly`() {
+        sut = LighthouseState
+        sut.setDeviceList(emptyList())
+
+        `when`(SHELL_BYEBYE_PACKET.uuid).thenReturn(UUID.randomUUID())
+        val updatedList = sut.parseMediaPacket(SHELL_BYEBYE_PACKET)
+        assertTrue(updatedList.isEmpty())
+    }
+
+    object Fixtures {
+        val SHELL_ALIVE_PACKET = mock(AliveMediaPacket::class.java)
+        val SHELL_UPDATE_PACKET = mock(UpdateMediaPacket::class.java)
+        val SHELL_BYEBYE_PACKET = mock(ByeByeMediaPacket::class.java)
+    }
+}

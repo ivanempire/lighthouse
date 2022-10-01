a248c7d768019698ff166f4564e841bc399f5a75
Ivan Melnikov <ivanempire@gmail.com>
2022-10-01 01:06:41 -0700

    Optimizing final list emissions

--- lighthouse/src/main/java/com/ivanempire/lighthouse/core/RealLighthouseClient.kt
+++ lighthouse/src/main/java/com/ivanempire/lighthouse/core/RealLighthouseClient.kt
@@ -5,8 +5,11 @@ import com.ivanempire.lighthouse.models.devices.AbridgedMediaDevice
 import com.ivanempire.lighthouse.models.search.SearchRequest
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flattenMerge
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 
 /** Specific implementation of [LighthouseClient] */
@@ -15,10 +18,17 @@ internal class RealLighthouseClient(
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : LighthouseClient {
 
+    @OptIn(FlowPreview::class)
     override fun discoverDevices(searchRequest: SearchRequest): Flow<List<AbridgedMediaDevice>> {
-        return discoveryManager.createNewDeviceFlow(searchRequest)
-            .combine(discoveryManager.createStaleDeviceFlow()) { newDeviceList, oldDeviceList ->
-                newDeviceList - oldDeviceList.toSet()
-            }.flowOn(dispatcher)
+        val foundDevicesFlow = discoveryManager.createNewDeviceFlow(searchRequest)
+        val lostDevicesFlow = discoveryManager.createStaleDeviceFlow()
+
+        // flattenMerge() causes the overall Flow to emit every second (due to the periodic check
+        // for stale devices), however, by dropping empty lists the consumers will only receive
+        // an updated list when there's a change: new device, an update, or a device goes offline
+        return flowOf(foundDevicesFlow, lostDevicesFlow)
+            .flattenMerge()
+            .filter { it.isNotEmpty() }
+            .flowOn(dispatcher)
     }
 }

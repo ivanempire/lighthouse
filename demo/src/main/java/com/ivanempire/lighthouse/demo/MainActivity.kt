d443cac83054493f7b3c5daf2784b82aba092cca
Sean Proctor <sproctor@gmail.com>
2023-03-23 08:35:20 -0400

    Add TTL to demo

--- demo/src/main/java/com/ivanempire/lighthouse/demo/MainActivity.kt
+++ demo/src/main/java/com/ivanempire/lighthouse/demo/MainActivity.kt
@@ -11,11 +11,17 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.ivanempire.lighthouse.LighthouseClient
 import com.ivanempire.lighthouse.models.devices.AbridgedMediaDevice
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.map
 
 class MainActivity : ComponentActivity() {
 
@@ -29,8 +35,10 @@ class MainActivity : ComponentActivity() {
             .setRetryCount(2)
             .build()
 
+        val devicesFlow = lighthouseClient.discoverDevices()
+            .map { devices -> devices.sortedBy { it.uuid } }
+
         setContent {
-            val devicesFlow = remember { lighthouseClient.discoverDevices() }
             val devices = devicesFlow.collectAsState(emptyList())
 
             Surface(modifier = Modifier.fillMaxSize()) {
@@ -50,8 +58,17 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeviceListItem(device: AbridgedMediaDevice) {
+    var currentTime by remember { mutableStateOf(System.currentTimeMillis()) }
+    val ttl = device.cache - (currentTime - device.latestTimestamp) / 1000
     ListItem(
-        headlineText = { Text(device.host.toString()) },
-        supportingText = { Text(device.uuid.toString()) },
+        headlineText = { Text(device.location.toString()) },
+        supportingText = { Text(device.uuid) },
+        trailingContent = { Text(ttl.toString())}
     )
+    LaunchedEffect(Unit) {
+        while (true) {
+            currentTime = System.currentTimeMillis()
+            delay(1000)
+        }
+    }
 }

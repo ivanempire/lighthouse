41f8ac357c467eea3006c6480358a0811d9734c2
Sean Proctor <sproctor@gmail.com>
2023-04-07 07:25:00 -0400

    remove `flowOn` from client

--- lighthouse/src/main/java/com/ivanempire/lighthouse/core/RealLighthouseClient.kt
+++ lighthouse/src/main/java/com/ivanempire/lighthouse/core/RealLighthouseClient.kt
@@ -3,10 +3,7 @@ package com.ivanempire.lighthouse.core
 import com.ivanempire.lighthouse.LighthouseClient
 import com.ivanempire.lighthouse.models.devices.AbridgedMediaDevice
 import com.ivanempire.lighthouse.models.search.SearchRequest
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 
 /** Specific implementation of [LighthouseClient] */
 internal class RealLighthouseClient(

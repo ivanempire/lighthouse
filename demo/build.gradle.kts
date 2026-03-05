7ac28437e6a45ac8916a26c7fb021028b6602a86
Ivan Melnikov <ivanempire@gmail.com>
2026-03-04 23:09:52 -0800

    TOML cleanup + Kotlin DSL move

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -34,8 +34,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
 }
+
+kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
 
 dependencies {
     // Core Android libraries

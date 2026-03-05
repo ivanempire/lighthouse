56ec610b8a0cb1a1824e8ac1633ad0e257ad6762
Ivan Melnikov <ivanempire@gmail.com>
2026-03-04 20:59:26 -0800

    Removing buildSrc to handle AGP 9.0 upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.ktfmt)
 }
 
